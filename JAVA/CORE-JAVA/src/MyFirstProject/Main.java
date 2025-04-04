f33b54147c90dc6699b78804e71c45be4a50791f
Manqasana-Lepota <nqhasy@gmail.com>
2025-04-04 13:30:20 +0200

    First commit to CORE-JAVA

---
+++ JAVA/CORE-JAVA/src/MyFirstProject/Main.java
@@ -0,0 +1,13 @@
+package MyFirstProject;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // This is my first Java Program
+
+        System.out.println("I like music!");
+        System.out.println("It's really nice to listen to good music");
+        System.out.println("Can you please play nice gospel song for me?");
+        System.out.println("*****************GOOD BYE**********************");
+    }
+}
