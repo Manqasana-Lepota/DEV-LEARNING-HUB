8f7e54c60f3031b6bcb638f831110bd809fb05f1
Manqasana-Lepota <nqhasy@gmail.com>
2025-05-21 13:21:54 +0200

    Initial commit: added JavaScript folder

---
+++ TernaryOperator/index.js
@@ -0,0 +1,13 @@
+// TERNARY OPERATOR
+
+let age = 12;
+let message = age >= 18 ? "You're an adult" : "You're a minor";
+console.log(message);
+
+let time = 9;
+let greeting = time < 12 ? "Good Morning" : "Good Afternoon";
+console.log(greeting);
+
+let isStudent = false;
+let msg = isStudent ? "You are a student" : "You are NOT a student"
+console.log(msg);
