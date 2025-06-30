cf6f03cea3633f4e6c01630e350e4318ecd56800
Manqasana-Lepota <59992928+Manqasana-Lepota@users.noreply.github.com>
2025-06-30 20:51:42 +0200

    added jJavascript folder

---
+++ JAVASCRIPT/Lesson1/user-input/user-input.js
@@ -0,0 +1,4 @@
+// How to accept user input
+
+// 1. Easy Way = window prompt
+// 2. Professional Way = HTML textbox
---
+++ JAVASCRIPT/Lesson1/variables/index.js
@@ -0,0 +1,14 @@
+// Variable = A container that stores a value.
+//            Behaves as if it were the value it contains
+
+
+// 1. declaration  let x;
+// 2. assignment   x = 100;
+
+let fullname = "Manqasana Lepota";
+let age = 25;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `My name is ${fullname}`;
+document.getElementById("p2").textContent = `I'm ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
